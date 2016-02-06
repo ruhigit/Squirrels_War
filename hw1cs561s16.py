baaf6a44a0835176e32aeae730399fb297ce21f1
ruhigit <ruhiagarwal@live.com>
2016-02-06 10:54:54 -0800

    program skeleton

---
+++ hw1cs561s16.py
@@ -0,0 +1,55 @@
+'''
+python hw1cs561s16.py –i inputFile
+The game board is a 5x5 grid representing territories the squirrel
+warriors will trample.
+The game ends when all the squares are occupied, because no more moves are left
+'''
+import sys
+
+# Check if square can be raided or sneaked
+def check_square():
+	#If the square is horizontally or vertically adjacent to an existing selfowned piece, it’s a Raid.
+
+	#Otherwise it’s a Sneak
+
+	end
+
+
+#A new piece is created in the target square,and any enemy pieces adjacent 
+#to the target square are turned to the player’s side.
+def raid():
+	end
+
+
+#A new piece is created in the target square
+def sneak():
+	end
+
+
+#E(s) = Total_Value_Player - Total_Value_Opponent
+def evaluation():
+	end
+
+
+
+def tie_breaker():
+	end
+
+
+def GreedyBestfirstSearch():
+	end
+
+#decode the input file for part1
+def decode_input_part1():
+	
+	end
+#decode the input file for part2
+def decode_input_part2():
+	
+	end
+
+def main():
+	#create the 5 X 5 board
+	board = [[0 for x in range(5)] for x in range(5)] 
+
+	end
